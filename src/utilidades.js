c5a65db68df6473c06528d6ee020c678884e5857
Vladimir Rodrigues <rodrigues.vladimir@outlook.com>
2023-09-01 10:38:46 -0300

    e-commerce_v1.1

---
+++ src/utilidades.js
@@ -0,0 +1,217 @@
+export const catalogo = [
+    {
+        id: 1,
+        marca: "Zara",
+        nome: "Camisa Larga com Bolsos",        
+        preco: 70,
+        imagem: "product-1.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 2,
+        marca: "Zara",
+        nome: "Casaco Preto com La",        
+        preco: 85,
+        imagem: "product-2.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 3,
+        marca: "Zara",
+        nome: "Jaqueta com Efeito Camurca",        
+        preco: 60,
+        imagem: "product-3.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 4,
+        marca: "Zara",
+        nome: "Sobretudo em Mescla de La",        
+        preco: 160,
+        imagem: "product-4.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 5,
+        marca: "Zara",
+        nome: "Camisa Larga Acolchoada de Veludo Cotele",        
+        preco: 110,
+        imagem: "product-5.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 6,
+        marca: "Zara",
+        nome: "Casaco de La com Botoes",        
+        preco: 170,
+        imagem: "product-6.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 7,
+        marca: "Zara",
+        nome: "Casaco com Botoes",        
+        preco: 75,
+        imagem: "product-7.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 8,
+        marca: "Zara",
+        nome: "Colete Comprido com cinto",        
+        preco: 88,
+        imagem: "product-8.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 9,
+        marca: "Zara",
+        nome: "Camisa Larga com Bolsos",        
+        preco: 70,
+        imagem: "product-1.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 10,
+        marca: "Zara",
+        nome: "Casaco Preto com La",        
+        preco: 85,
+        imagem: "product-2.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 11,
+        marca: "Zara",
+        nome: "Jaqueta com Efeito Camurca",        
+        preco: 60,
+        imagem: "product-3.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 12,
+        marca: "Zara",
+        nome: "Sobretudo em Mescla de La",        
+        preco: 160,
+        imagem: "product-4.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 13,
+        marca: "Zara",
+        nome: "Camisa Larga Acolchoada de Veludo Cotele",        
+        preco: 110,
+        imagem: "product-5.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 14,
+        marca: "Zara",
+        nome: "Casaco de La com Botoes",        
+        preco: 170,
+        imagem: "product-6.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 15,
+        marca: "Zara",
+        nome: "Casaco com Botoes",        
+        preco: 75,
+        imagem: "product-7.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 16,
+        marca: "Zara",
+        nome: "Colete Comprido com cinto",        
+        preco: 88,
+        imagem: "product-8.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 17,
+        marca: "Zara",
+        nome: "Camisa Larga com Bolsos",        
+        preco: 70,
+        imagem: "product-1.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 18,
+        marca: "Zara",
+        nome: "Casaco Preto com La",        
+        preco: 85,
+        imagem: "product-2.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 19,
+        marca: "Zara",
+        nome: "Jaqueta com Efeito Camurca",        
+        preco: 60,
+        imagem: "product-3.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 20,
+        marca: "Zara",
+        nome: "Sobretudo em Mescla de La",        
+        preco: 160,
+        imagem: "product-4.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 21,
+        marca: "Zara",
+        nome: "Camisa Larga Acolchoada de Veludo Cotele",        
+        preco: 110,
+        imagem: "product-5.jpg",
+        feminino: false, 
+    },
+
+    {
+        id: 22,
+        marca: "Zara",
+        nome: "Casaco de La com Botoes",        
+        preco: 170,
+        imagem: "product-6.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 23,
+        marca: "Zara",
+        nome: "Casaco com Botoes",        
+        preco: 75,
+        imagem: "product-7.jpg",
+        feminino: true, 
+    },
+
+    {
+        id: 24,
+        marca: "Zara",
+        nome: "Colete Comprido com cinto",        
+        preco: 88,
+        imagem: "product-8.jpg",
+        feminino: true, 
+    },
+];
